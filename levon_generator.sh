c8975e20a1310cc5f872cefce7e5cadfa7534e57
Levon_Inst <ylight.am@mail.ru>
2014-09-17 17:43:13 +0400

    some working with html

--- levon_generator.sh
+++ levon_generator.sh
@@ -12,18 +12,18 @@ init()
 ##Software Informacion
 software_info()
 {
-	echo "<h1>Software Info.</h1>" >> $FILE_NAME
+	echo "<h1><i><font color=":#00FFCC">Software Info</h1></font></i>" >> $FILE_NAME
 }
 ##Showing user name
 show_username()
 {	
-	echo "<i><font size="=3">User: </i><b>"`whoami`"</b>" >> $FILE_NAME   
+	echo "<i><b><font size="2">User: </i><b>"`whoami`"</font><i></b>" >> $FILE_NAME   
 		echo "<br/>" >> $FILE_NAME
 }
 ##What operating system using
 operating_system()
 {	
-	echo "<i>Description: </i><b>"`lsb_release -d  | cut -f2`"</b>" >> $FILE_NAME     
+	echo "Description:"`lsb_release -d  | cut -f2` >> $FILE_NAME     
 
 		echo "<br/>" >> $FILE_NAME
 }
@@ -61,7 +61,7 @@ desktop_enviroment()
 ##Hardware Information
 hardware_info()
 {
-	echo "<h1>Hardware Info</h1>" >> $FILE_NAME
+	echo "<h1><font color=":#00FFCC">Hardware Info</h1></font>" >> $FILE_NAME
 }
 ##Ram
 memory()
@@ -97,30 +97,13 @@ disk_size()
 ##Information about pc network conections
 network_info()
 {
-	echo "<h2> Network Info:</h2>" >> $FILE_NAME
+	echo "<h1><font color=":#00FFCC"> Network Info</h1></font>" >> $FILE_NAME
 }
 ##Router or a proxy server that routes between network
 gateway()
 {
-	echo "Gateway"  `route -n | grep 'UG[ \t]' | awk '{print $2}' ` >>$FILE_NAME
-		echo "</br>" >> $FILE_NAME
+	echo "<b><i><font size="2">Gateway"  `route -n | grep 'UG[ \t]' | awk '{print $2}' `"</font></i></b>" >>$FILE_NAME
 }
-#echo "<b>"`/sbin/ifconfig | grep eth | awk ' {print $1'}`"</b>" >> $FILE_NAME
-#echo "</br>" >> $FILE_NAME
-#echo "<i>MacAdress:</i>" >> $FILE_NAME `/sbin/ifconfig -a | grep HWaddr >macadress
-#head -1 macadress |  awk ' {print $5 '}` >> $FILE_NAME
-#echo "<h3>lo</h3>" >>$FILE_NAME
-#echo "<i>Ip Adress:</i>"  `/sbin/ifconfig lo |grep "inet addr" | cut -b 21-` >> $FILE_NAME
-#wifi=`head -2 macadress`
-#if [ "$wifi" != null ]
-#then
-#echo "WIfI KA" >> $FILE_NAME
-#else
-#echo "No Wifi conection" >> $FILE_NAME
-#fi
-#rm macadress
-
-
 ##eth-Ethernet is a family of computer networking technologies for local area (LAN) and larger network
 ##lo- links two or more devices using some wireless distribution method
 ##wlan
@@ -129,14 +112,14 @@ connections()
 	LEVON=`/sbin/ifconfig | tr -s ' ' | cut -f 1 -d ' ' `
 		for i in $LEVON
 			do
-
-				echo $i >> $FILE_NAME
+				echo "</br>" >> $FILE_NAME   
+					echo "<h2>$i</h2>" >> $FILE_NAME
 					echo "</br>" >> $FILE_NAME
 					HWadd=$( /sbin/ifconfig $i | grep HWaddr ) #>> $FILE_NAME
 					if [ -n "$HWadd" ]
 						then
 
-							echo "MacAdress:" `/sbin/ifconfig $i | grep  HWaddr | awk ' {print $5}'` >> $FILE_NAME 
+							echo "<i>MacAdress:</i>" `/sbin/ifconfig $i | grep  HWaddr | awk ' {print $5}'` >> $FILE_NAME 
 							echo "</br>" >> FILE_NAME
 							fi
 							INET=$(/sbin/ifconfig $i | grep "inet addr") #>> $FILE_NAME
@@ -180,28 +163,45 @@ show()
 
 }
 
-
-#Entry point
-main()
+##software main
+software_main()
 {
-	init
-		software_info
+	software_info
 		show_username
 		operating_system
 		kernel_architecture
 		kernel
 		codename
 		desktop_enviroment
-		hardware_info
+}
+
+##hardware main
+hardware_main()
+{
+	hardware_info
 		memory
 		processor
 		cpu_cores
 		disk_size
-		network_info
+}
+##network main
+network_main()
+{
+	network_info
 		gateway
 		connections
-		cat end.html >> $FILE_NAME
-		show 
+}
+
+
+#Entry point
+main()
+{
+	init
+		software_main
+		hardware_main
+		network_main 
+		cat  end.html >> $FILE_NAME
+		show  
 }
 
 
